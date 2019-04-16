420c648d65c69a90218d5237201255cb02e9572e
Greg Cormier <greg.cormier@dfo.mpo.ca>
2019-04-16 14:42:03 -0400

    a

--- nodeprep.sh
+++ nodeprep.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-touch /volatile/prep1.garb
+touch prep1.garb
+sudo touch prep2.garb
 
 /bin/sh -c "yum install -y kernel-headers --disableexcludes=all; yum -y install make gcc gcc-c++ gcc-gfortran cmake zlib-devel openmpi openmpi-devel fftw fftw-devel gsl gsl-devel gmp environment-modules; source /etc/profile.d/modules.sh; module add mpi/openmpi-$(uname -i); module load mpi/openmpi-$(uname -i)"
 
